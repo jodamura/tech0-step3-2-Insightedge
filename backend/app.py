c1ee516cdb385206a4e5a4cf4873774b6fca51aa
nakai <nakaishogo0627@gmail.com>
2023-12-15 10:57:34 +0900

    繋ぎ込み部分コード作成しました

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+stores = [
+    {
+        "name": "Store1",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/store")
+def get_stores():
+    return {"stores": stores}
